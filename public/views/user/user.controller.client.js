d8cd67a6753f1e207fd15258bba825e928daba87
RuihanWang <wangruihan01@gmail.com>
2016-12-15 21:47:21 -0500

    guo

--- public/views/user/user.controller.client.js
+++ public/views/user/user.controller.client.js
@@ -67,6 +67,7 @@
                         username:username,
                         password:password
                     };
+                    console.log("guo1");
                 var pro = UserService.login(user);
                     pro
                         .then(function(user){
@@ -90,19 +91,19 @@
 
     function RegisterController( $routeParams,UserService, $location) {
         var vm = this;
-        console.log("jdc")
         vm.register = register;
-        function register(username,password,firstname, lastname) {
+        function register(username, password, firstname, lastname) {
             var user = {username: username, password: password, firstName: firstname, lastName: lastname};
-
+console.log("guo1")
 
             UserService
                 .createUser(user)
-                .then(function(user) {
+                .then(function (user) {
                     console.log(user);
                     $location.url("/user/" + user._id);
                 });
         }
+
 
         function RegisterSuccess(user) {
             $location.url("/user/" + user._id);
